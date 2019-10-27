137d8520c3e5a63af339924873e0d6aedc66654c
Ethan <sk18181@bristol.ac.uk>
2019-10-27 01:48:59 +0000

    included serial shit

--- HOST/host.py
+++ HOST/host.py
@@ -1,23 +1,29 @@
 # Download the helper library from https://www.twilio.com/docs/python/install
 from twilio.rest import Client
-
+import serial
 account_sid = 'ACf627d2336806adfa6b0475e9739babaf'
 auth_token = '1abf3c0296584cc5f7114359313f048f'
 client = Client(account_sid, auth_token)
+def call():
+    call = client.calls.create(
+                            url='http://vps736330.ovh.net:8000/resp.xml',
+                            to='+447342178041',
+                            from_='+447723569692'
+                        )
 
-call = client.calls.create(
-                        url='http://vps736330.ovh.net:8000/resp.xml',
-                        to='+447342178041',
+    print(call.sid)
+
+    message = client.messages \
+                    .create(
+                        body="Join Earth's mightiest heroes. Like Kevin Bacon.",
+                        to='+447934359814',
                         from_='+447723569692'
                     )
 
-print(call.sid)
+    print(message.sid)
 
-# message = client.messages \
-#                 .create(
-#                     body="Join Earth's mightiest heroes. Like Kevin Bacon.",
-#                     to='+447934359814',
-#                     from_='+447723569692'
-#                  )
+with serial.Serial('/dev/ttyACM0',115200,timeout=1) as ser:
+    line=ser.readline()
 
-# print(message.sid)
+    if line=="help":
+        call()
